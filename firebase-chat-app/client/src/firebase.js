64bd76d2108ff4fb2fdca09f47bdf2df76d10103
Ellie <elliem40499@gmail.com>
2025-05-22 14:19:58 -0400

    init

--- firebase-chat-app/client/src/firebase.js
+++ firebase-chat-app/client/src/firebase.js
@@ -2,14 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC01Pb4utJs6RpR8vDzaFUgWp3ndUESDQ8",
-  authDomain: "chat-app-a09ed.firebaseapp.com",
-  projectId: "chat-app-a09ed",
-  storageBucket: "chat-app-a09ed.firebasestorage.app",
-  messagingSenderId: "904641823108",
-  appId: "1:904641823108:web:42a72bcde38e8185e2ddd1"
-};
+const firebaseConfig = require('../../keys/keys.json');
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
